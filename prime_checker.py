3922bf3b5ca33b4f1a24f758331be6c3566d9fa0
Tran Duc Thang <thang2k6adu@gmail.com>
2024-12-04 10:14:18 +0700

    Optimalize prime number checking function

--- prime_checker.py
+++ prime_checker.py
@@ -5,3 +5,12 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n + 1)
+    primes[0], primes[1] = False, False
+    for i in range(2, int(n**0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+    return [x for x in range(n + 1) if primes[x]]
+
